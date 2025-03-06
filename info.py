faaa7efc6d4b781044f406c2de987138763df8d8
Nihal Singh <142614932+Emperorns@users.noreply.github.com>
2025-03-06 16:09:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -128,7 +128,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkshortify.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '7d706f6d7c95ff3fae2f2f40cff10abdc0e012e9')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/purplebotz') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/disneysworl_d') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
