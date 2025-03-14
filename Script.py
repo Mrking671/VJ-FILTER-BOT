c119b94cca0c38cf8cf8e4535dc417c3d08cb0ae
Nihal Singh <142614932+Emperorns@users.noreply.github.com>
2025-03-14 17:59:36 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -260,13 +260,7 @@ IMDb Data:
 ● Genres: {genres}
 ● Year: <a href={url}/releaseinfo>{year}</a>
 ● Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
-● Languages : <code>{languages}</code>
-● RunTime: {runtime} Minutes
 ● Release Info : {release_date}
-● Countries : <code>{countries}</code>
-
-
-⏰Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
 
 Requested by : {message.from_user.mention}</b>"""
     
